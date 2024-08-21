dea79c390e28646740ada325109a6fa5f81fd9b7
Bastian8004 <123553744+Bastian8004@users.noreply.github.com>
2024-08-21 17:37:44 +0200

    Update views.py

--- InstallHome/views.py
+++ InstallHome/views.py
@@ -48,18 +48,16 @@ def gallery(request):
     try:
         albums = paginator.page(page)
     except PageNotAnInteger:
-        albums = paginator.page(1) # If page is not an integer, deliver first page.
+        albums = paginator.page(1) 
     except EmptyPage:
-        albums = paginator.page(paginator.num_pages) # If page is out of range (e.g.  9999), deliver last page of results.
+        albums = paginator.page(paginator.num_pages) 
 
     return render(request, 'post_list.html', { 'albums': list })
 
 class AlbumDetail(DetailView):
      model = Album
      def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
         context = super(AlbumDetail, self).get_context_data(**kwargs)
-        # Add in a QuerySet of all the images
         context['images'] = AlbumImage.objects.filter(album=self.object.id)
         return context
 
@@ -81,8 +79,6 @@ def ofert_list(request):
     return render(request, 'ofert_list.html', {'ofertys': ofertys})
 
 def ofert_detail(request, pk):
-    # Pobieramy kategorię na podstawie przekazanego ID
     oferty = Oferty.objects.get(pk=pk)
-    # Pobieramy wszystkie dodatkowe modele powiązane z tą kategorią
     cenniks = Cennik.objects.filter(category=oferty)
     return render(request, 'ofert_detail.html', {'oferta': oferty, 'cenniks': cenniks})
